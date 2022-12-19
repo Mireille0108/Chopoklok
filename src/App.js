961e8acd15c46fe811f403e74d7c16bbae517fea
kambangsinclaire <kambangsinclaire@gmail.com>
2022-12-19 15:52:47 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './modules/components/Navbar';
 import './App.css';
+import Footer from './modules/components/Footer';
 
 function App() {
   return (
     <section>
       {/* Navbar section */}
       <Navbar />
+      <Footer />
     </section>
   );
 }
---
+++ src/modules/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <header className="bg-blue-500">
+            <h1>copyright@chopoklok 2022</h1>
+        </header>
+    );
+}
+
+export default Footer;
