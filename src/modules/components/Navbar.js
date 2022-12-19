6284e49fa7c7bf077985404a0b534fbb866500c5
kambangsinclaire <kambangsinclaire@gmail.com>
2022-12-19 15:30:44 +0100

    setup taildwindcs

---
+++ src/modules/components/Navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => {
+    return (
+        <header className="bg-blue-500">
+            <nav className="w-full">
+                <ul>
+                    <li>
+                        <a>Home</a>
+                    </li>
+                    <li>
+                        <a>Orders</a>
+                    </li>
+                    <li>
+                        <a>About</a>
+                    </li>
+                    <li>
+                        <a>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
