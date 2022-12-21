3843339585de7618fc21c1d7ed6ab057f62fdda6
Mireille0108 <EmbohMireill07@gmail.com>
2022-12-21 06:44:43 -0800

    added a new file

---
+++ src/modules/components/Menu.js
@@ -0,0 +1,19 @@
+const Menu = () => {
+    return(
+        <section>
+            <header>
+                <h1>Today's Menu</h1>
+                <span></span>
+            </header>
+            <div className="menu-items-container">
+                <span></span>
+<div className="menu-item"></div>
+<span>menu 1</span>
+<div className="menu-item"></div>
+<div className="menu-item"></div>
+<div className="menu-item"></div>
+<div className="menu-item"></div>
+            </div>
+        </section>
+    )
+}
